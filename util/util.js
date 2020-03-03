4cd029ece8b950e97b0efe577e050386ead39773
wukai900610 <wukai900610@gmail.com>
2020-03-03 14:08:47 +0800

    数组合并

--- util/util.js
+++ util/util.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 
 let storageData = {};
 const util = {
-    upFile:function (req, res, next, callback) {
+    upFile: function(req, res, next, callback) {
         var form = new multiparty.Form();
 
         form.parse(req, function(err, fields, files) {
@@ -31,15 +31,14 @@ const util = {
         });
     },
     myHttp: function(url, config) {
-        config = config || {};
-        let options = {
-            method: config.method || 'get',
-        };
+        let options = Object.assign({
+            method: 'get'
+        }, config);
 
         let promise = new Promise(function(resolve, rejecte) {
             let req = http.request(url, options, (res) => {
                 res.setEncoding('utf8');
-                 let finalData = '';
+                let finalData = '';
                 res.on('data', (d) => {
                     finalData += d;
                 });
@@ -54,16 +53,14 @@ const util = {
         return promise;
     },
     myHttps: function(url, config) {
-        config = config || {};
-        let options = {
-            method: config.method || 'get',
-            data:config.data
-        };
+        let options = Object.assign({
+            method: 'get'
+        }, config);
 
         let promise = new Promise(function(resolve, rejecte) {
             let req = https.request(url, options, (res) => {
                 res.setEncoding('utf8');
-                 let finalData = '';
+                let finalData = '';
                 res.on('data', (d) => {
                     finalData += d;
                 });
@@ -77,23 +74,23 @@ const util = {
         })
         return promise;
     },
-    getToken: async function () {
-        let result = await util.myHttps('https://oapi.dingtalk.com/gettoken?appkey=dingcpkf8oosoicxx57x&appsecret='+encodeURI('XfEiNbTU-qLNMU309oLlZ67vvnqDWUgVN2gIEdfTvS-uMQ2lixLYN_WpBsS6U9Dg'));
+    getToken: async function() {
+        let result = await util.myHttps('https://oapi.dingtalk.com/gettoken?appkey=dingcpkf8oosoicxx57x&appsecret=' + encodeURI('XfEiNbTU-qLNMU309oLlZ67vvnqDWUgVN2gIEdfTvS-uMQ2lixLYN_WpBsS6U9Dg'));
         result = JSON.parse(result);
         return result.access_token;
     },
-    getUserinfo: async function (token,code) {
-        let result = await util.myHttps('https://oapi.dingtalk.com/user/getuserinfo?access_token='+token+'&code='+code);
+    getUserinfo: async function(token, code) {
+        let result = await util.myHttps('https://oapi.dingtalk.com/user/getuserinfo?access_token=' + token + '&code=' + code);
         result = JSON.parse(result);
         return result;
     },
-    setData:function (key, value) {
+    setData: function(key, value) {
         storageData[key] = value;
     },
-    getData:function (key) {
+    getData: function(key) {
         return storageData[key];
     },
-    delData:function (key) {
+    delData: function(key) {
         delete storageData[key];
     }
 }
