4c16d5c8d7b0b0b9a8d9b910c01d1914e15d9cc0
wukai900610 <wukai900610@gmail.com>
2020-06-03 10:19:12 +0800

    从单个生成变成全部生成

--- uaecEmail/index.js
+++ uaecEmail/index.js
@@ -2,39 +2,45 @@ var fs = require('fs');
 var util = require('../util/util.js');
 var config = require('./config.js');
 
-let yearMonthDay = 'nigeria-buildingmaterial-doczj'
-let obj = config[yearMonthDay]
+// let yearMonthDay = 'nigeria-buildingmaterial-doczj'
+// let obj = config[yearMonthDay]
 
 let filePath = './uaecEmail/'
 let fileArr = ['all.html', 'PushBuyer.html', 'CheckedSupplier.html', 'Supplier.html', 'SupplierReceived.html', 'Buyer.html', 'BuyerRecived.html']
 
-let fileIndex = 0
+// let fileIndex = 0;
+for( let yearMonthDay in config){
+    let fair = config[yearMonthDay];
 
-function doFile() {
-    fs.readFile(filePath + fileArr[fileIndex], 'utf8', function(err, data) {
-        // console.log(data);
-        for(var i=0;i<obj.length;i++){
-            // console.log(item);
-            let item = obj[i]
-            var re = new RegExp("{{" + item.key + "}}", "g");
-            data = data.replace(re, item.value);
-        }
+    // 创建对应邮件项目目录
+    util.mkdir(filePath + 'dist/' + yearMonthDay)
 
-        util.mkdir(filePath + 'dist/' + yearMonthDay)
-        fs.writeFile(filePath + 'dist/' + yearMonthDay + '/' +fileArr[fileIndex], data, function(err) {
-            if (err){
-                console.log('文件'+fileIndex+'写操作失败');
-            }else{
-                console.log('文件'+fileIndex+'操作成功');
-                fileIndex++;
-                doFile()
-
-                if(fileIndex == fileArr.length-1){
-                    console.log('ok:'+fileIndex);
-                }
-            }
+    fileArr.map((item,index)=>{
+        fs.readFile(filePath + fileArr[index], 'utf8', (err, fileData)=>{
+            writeFile(fileData,fair,yearMonthDay,index)
         });
-    });
+    })
 }
 
-doFile();
+function writeFile(fileData,fair,yearMonthDay,index) {
+    for(var i=0;i<fair.length;i++){
+        // console.log(item);
+        let item = fair[i]
+        var re = new RegExp("{{" + item.key + "}}", "g");
+        fileData = fileData.replace(re, item.value);
+    }
+
+    // return false;
+    fs.writeFile(filePath + 'dist/' + yearMonthDay + '/' +fileArr[index], fileData, function(err) {
+        if (err){
+            console.log(yearMonthDay+':文件名'+fileArr[index]+'写操作失败');
+        }else{
+            // console.log(yearMonthDay+':文件'+index+'操作成功');
+            // doFile()
+            //
+            // if(fileIndex == fileArr.length-1){
+            //     console.log('ok:'+fileIndex);
+            // }
+        }
+    });
+}
